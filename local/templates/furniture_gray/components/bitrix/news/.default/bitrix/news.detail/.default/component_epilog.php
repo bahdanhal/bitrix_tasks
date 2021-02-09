dcccfe365df687a59b55c0dedc08a53b2297847f
bahdan.hal <bahdan.hal@hotmail.com>
2021-02-09 10:12:33 +0300

    ex2-108

--- local/templates/furniture_gray/components/bitrix/news/.default/bitrix/news.detail/.default/component_epilog.php
+++ local/templates/furniture_gray/components/bitrix/news/.default/bitrix/news.detail/.default/component_epilog.php
@@ -1,4 +1,23 @@
-<?if(!defined("B_PROLOG_INCLUDED") or B_PROLOG_INCLUDED!==true) die();
-if($arResult['CANONICAL']){
-    $APPLICATION->SetPageProperty("canonical", $arResult['CANONICAL']);
+<?if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true) die();
+if(CModule::IncludeModule("iblock")){
+    if (is_set($arParams["CANONICAL_ID"]) and $arParams["CANONICAL_ID"] > 0) {
+        $select = array(
+            'ID',
+            'IBLOCK_ID',
+            'NAME',
+            'PROPERTY_NEWS',
+        );
+       $filter = array(
+          "IBLOCK_ID" => $arParams["CANONICAL_ID"],
+          "PROPERTY_NEWS" => $arResult["ID"]
+       );
+    
+       $iterator = CIBlockElement::GetList(array(), $filter, false, array(), $select);
+       if ($result = $iterator->GetNext()) {
+           if($result["NAME"]){
+                $APPLICATION->SetPageProperty("canonical", $result["NAME"]);
+           }
+       }
+    }
+    
 }

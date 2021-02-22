4be2edd289e04f10cecbec3c2b698c837423f039
bahdan.hal <bahdan.hal@hotmail.com>
2021-02-22 17:33:34 +0300

    ex2-108

--- local/templates/furniture_gray/components/bitrix/news/.default/bitrix/news.detail/.default/component_epilog.php
+++ local/templates/furniture_gray/components/bitrix/news/.default/bitrix/news.detail/.default/component_epilog.php
@@ -1,23 +1,6 @@
 <?if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true) die();
-if(CModule::IncludeModule("iblock")){
-    if (is_set($arParams["CANONICAL_ID"]) and $arParams["CANONICAL_ID"] > 0) {
-        $select = array(
-            'ID',
-            'IBLOCK_ID',
-            'NAME',
-            'PROPERTY_NEWS',
-        );
-       $filter = array(
-          "IBLOCK_ID" => $arParams["CANONICAL_ID"],
-          "PROPERTY_NEWS" => $arResult["ID"]
-       );
+if($arResult['CANONICAL']){
+
+    $APPLICATION->SetPageProperty("canonical", $arResult['CANONICAL']);
     
-       $iterator = CIBlockElement::GetList(array(), $filter, false, array(), $select);
-       if ($result = $iterator->GetNext()) {
-           if($result["NAME"]){
-                $APPLICATION->SetPageProperty("canonical", $result["NAME"]);
-           }
-       }
-    }
-    
-}
+} 
---
+++ local/templates/furniture_gray/components/bitrix/news/.default/bitrix/news.detail/.default/result_modifier.php
@@ -0,0 +1,22 @@
+<?if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true) die();
+if (is_set($arParams["CANONICAL_ID"]) and $arParams["CANONICAL_ID"] > 0) {
+    $select = array(
+        'ID',
+        'IBLOCK_ID',
+        'NAME',
+        'PROPERTY_NEWS',
+    );
+    $filter = array(
+        "IBLOCK_ID" => $arParams["CANONICAL_ID"],
+        "PROPERTY_NEWS" => $arResult["ID"]
+    );
+
+    $iterator = CIBlockElement::GetList(array(), $filter, false, array(), $select);
+    if ($result = $iterator->GetNext()) {
+        if($result["NAME"]){
+            $arResult["CANONICAL"] = $result["NAME"]; 
+            $this->getComponent()->SetResultCacheKeys(array('CANONICAL'));
+        }
+    }
+}
+    

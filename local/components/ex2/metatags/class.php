933bbce20660d205150057e09d54de1a529c6fb5
bahdan.hal <bahdan.hal@hotmail.com>
2021-02-23 14:43:24 +0300

    ex2-94 update

--- local/components/ex2/metatags/class.php
+++ local/components/ex2/metatags/class.php
@@ -6,7 +6,7 @@ class Metatags extends CBitrixComponent
         if (!\Bitrix\Main\Loader::IncludeModule("iblock")){
             return false;
         }
-        if($this->StartResultCache(3600000)){
+        if($this->StartResultCache(3600000, $GLOBALS['APPLICATION']->GetCurPage())){
             $select = array(
                 'ID',
                 'IBLOCK_ID',
@@ -17,19 +17,23 @@ class Metatags extends CBitrixComponent
             $filter = array(
                 "NAME" => $GLOBALS['APPLICATION']->GetCurPage(),
             );
-            $this->arResult = Array("ELEMENT" => CIBlockElement::GetList(array(), $filter, false, array(), $select)->GetNext());
-            $this->SetResultCacheKeys(Array("ELEMENT"));
-            $this->EndResultCache();
-        }
-        $result = $this->arResult['ELEMENT'];
-        if (isset($result)) {
-            $db_props = CIBlockElement::GetProperty($result['IBLOCK_ID'], $result['ID'],"sort", "asc", array());
+            $element = CIBlockElement::GetList(array(), $filter, false, array(), $select)->GetNext();
+            if(!isset($element)){
+                $this->AbortResultCache();
+            }
+            $db_props = CIBlockElement::GetProperty($element['IBLOCK_ID'], $element['ID'],"sort", "asc", array());
             $PROPS = array();
             while($ar_props = $db_props->GetNext()){
                 $PROPS[$ar_props['NAME']] = $ar_props['VALUE'];
             }
-            $GLOBALS['APPLICATION']->SetPageProperty("title", $PROPS['TITLE']); 
-            $GLOBALS['APPLICATION']->SetPageProperty("description", $PROPS['DESCRIPTION']); 
+            $this->arResult = Array("TITLE" => $PROPS['TITLE'], "DESCRIPTION" => $PROPS['DESCRIPTION']);
+            $this->SetResultCacheKeys(Array("ELEMENT", "DESCRIPTION"));
+            $this->EndResultCache();
+        }
+
+        if (isset($this->arResult)) {
+            $GLOBALS['APPLICATION']->SetPageProperty("title", $this->arResult['TITLE']); 
+            $GLOBALS['APPLICATION']->SetPageProperty("description", $this->arResult['DESCRIPTION']); 
         }
     }
 }

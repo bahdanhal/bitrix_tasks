5c54ace4da36b3c0f288b9bfaeca6f31979b5dcc
bahdan.hal <bahdan.hal@hotmail.com>
2021-02-23 14:24:40 +0300

    ex2-94 update

--- local/php_interface/init.php
+++ local/php_interface/init.php
@@ -1,8 +1,10 @@
 <?
-AddEventHandler("main", "OnEpilog", function () {
+$eventManager = \Bitrix\Main\EventManager::getInstance();
+$eventManager->addEventHandler('main', 'OnEpilog', 'metatags');
+function metatags() {
     $GLOBALS['APPLICATION']->IncludeComponent(
         "ex2:metatags",
         "",
         Array(),
     );
-});
+}

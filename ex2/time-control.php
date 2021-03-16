738eb6a1a9a71323aa9bf7eca9324295c69d6ae8
bahdan.hal <bahdan.hal@hotmail.com>
2021-03-16 10:49:39 +0300

    ex2-10

--- ex2/time-control.php
+++ ex2/time-control.php
@@ -3,4 +3,4 @@ require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
 $APPLICATION->SetTitle("time_control");
 ?>Наибольшее время генерации страницы у&nbsp;<a href="http://localhost2/bitrix/admin/perfmon_hit_list.php?lang=ru&set_filter=Y&find_script_name=%2Fproducts%2Findex.php">/products/index.php</a>&nbsp;-&nbsp;2.3725. Её доля в общей статистике нагрузки - 23.52%<br>
  <br>
- На этой странице вызывается компонент bitrix.catalog. Его время работы 0.6773<br><?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
+ На этой странице вызывается компонент bitrix.catalog.section Его время работы&nbsp;0.1459, количество запросов 26<br><?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
